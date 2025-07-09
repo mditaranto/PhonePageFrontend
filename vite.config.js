ad7f718026b2992f6b939e218b0c99f835c8ddd4
mditaranto <matiasnahuel111@gmail.com>
2025-07-09 19:18:51 +0200

    Rutas estaticas

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+    base: "/PhonePageFrontend/",
     plugins: [react()],
-    base: "/PhonePageFrontend",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
